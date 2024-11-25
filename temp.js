cdbf9a05f1366581e80b7b54ea4ae1fb62622ba4
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-11-25 23:20:53 +0530

    black button working

--- temp.js
+++ temp.js
@@ -1,8 +1,30 @@
 "use strict";
 
+// Assigning variable for most commonly used node at global scope
 let selectedValue = 40;
 const valueBar = document.querySelector("#valueBar");
 const displayValue = document.querySelector("#displayValue");
+const penColor = document.querySelector('#color-picker');
+const gridBackground = document.querySelector('#grid-background');
+let board = document.querySelector(".board-container");
+
+//Set event listner for coloring cell on mouseover
+board.addEventListener('mouseover', (event) => {
+    let target = event.target;
+    let color = penColor.value;
+    if (target.className == 'cell' && mouseDown == true) {
+        target.style.backgroundColor = color;
+    }
+});
+
+//set event listener for coloring cell on click
+board.addEventListener('click', (event) => {
+    let target = event.target;
+    let color = penColor.value;
+    if (target.className == 'cell') {
+        target.style.backgroundColor = color;
+    }
+})
 
 //Set up event listener to get user's input value for grid size
 document.addEventListener("DOMContentLoaded", () => {
@@ -19,8 +41,6 @@ valueBar.addEventListener("input", (event) => {
     displayValue.textContent = selectedValue;
     createGridBoard();
 });
-
-let board = document.querySelector(".board-container");
 
 // make function to create grid based on users inputed value
 
@@ -41,10 +61,9 @@ function createGridBoard() {
         for (let j = 0; j < selectedValue; j++) {
             const cell = document.createElement('div');
             cell.classList.add("cell");
-
             gridRow.appendChild(cell);
-        }
-    }
+        };
+    };
 };
 
 //assign all buttons to a variable
@@ -55,24 +74,75 @@ let isOn = false;
 buttons.forEach((item) => {
     item.isOn = false;
 });
-// add event listener to turn off and on button on click
 
+/* Object.defineProperty(item, 'isOn', {
+    get() {
+        return this.isOn = false;
+    },
+    set(value) {
+        this.isOn = value;
+        if (value == true) {
+            if (item.id == 'black-pen') {
+                blackClicked();
+            }
+        }
+    }
+}) */
+
+// add event listener to turn off and on button on click
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         if (button.isOn === false) {
             console.log("first:-- " + button.isOn);
             button.isOn = true;
-            console.log('then: --'+ button.isOn);
+            console.log('then: --' + button.isOn);
             button.style.backgroundColor = 'greenyellow';
         }
         else if (button.isOn === true) {
             console.log("first:--" + button.isOn);
             button.isOn = false;
-            console.log('then: --'+ button.isOn);
+            console.log('then: --' + button.isOn);
             button.style.backgroundColor = 'white';
+        };
+        if (button.id == 'black-pen') {
+            if (button.isOn == true) {
+                penColor.value = '#000000';
+            } blackClicked();
         }
     });
 });
 
+//set variable to detect mousedown and mouseup and change value accordingly
 
-createGridBoard();
+let mouseDown = false;
+document.addEventListener('mousedown', () => {
+    mouseDown = true
+});
+document.addEventListener('mouseup', () => {
+    mouseDown = false;
+})
+
+//create functions for buttons clicked
+//black color button clicked
+function blackClicked() {
+    //   set isOn to false to white and rainbow     //  set bg of to white
+
+    buttons.forEach((item) => {
+        console.log(item.id + '&&' + item.isOn);
+        if (item.id == 'white-pen' || item.id == 'rainbow-pen' || item.id == 'eraser') {
+            item.isOn = false;
+            item.style.backgroundColor = 'white';
+        };
+    });
+
+    //   check if l or d ior f is enabled, and work accordingly
+};
+//create function to run the for each button enabled
+
+function runProgrammeByButtons(button) {
+
+};
+
+function runProgrammeByInputs() {
+
+}
